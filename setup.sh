5f3b097dc5f337262b7d4357db03ec11c07bacbd
Nicola Strappazzon C <nsc@nsc.local>
2021-12-03 21:58:33 +0100

    feat: more packages

--- setup.sh
+++ setup.sh
@@ -3,10 +3,59 @@ set -e
 
 sudo pacman -Syu --noconfirm
 
-if ! [ -x "$(command -v git)" ]; then
-	sudo pacman -S git --noconfirm
+PKGS=(
+	# Tools
+	'acpi'
+	'bash-completion'
+	'git'
+	'htop'
+	'networkmanager'
+	'ntp'
+	'openssh'
+	'rsync'
+	'tmux'
+	'iw'
+	'jq'
+	'aws-cli'
+	'traceroute'
+	'unrar'
+	'unzip'
+	'upower'
+	'vim'
+	'wget'
+    # Desktop
+	'xorg-server'
+    'xorg-xinit'
+    'bspwm'
+    'rofi'
+    'sakura'
+    'sxhkd'
+    # Fonts
+    'terminus-font'
+    'xorg-fonts-misc'
+    # Audio driver
+	'alsa-plugins'
+	'alsa-utils'
+	'pulseaudio-alsa'
+    # Touchpad driver
+    'libinput'
+    'xf86-input-libinput'
+)
+
+for PKG in "${PKGS[@]}"; do
+    sudo pacman -S "$PKG" --noconfirm --needed
+done
+
+if ! [ -x "$(command -v yay)" ]; then
+	cd /tmp/
+	git clone https://aur.archlinux.org/yay-git.git
+	cd yay-git/
+	makepkg -si
+	sudo yay -Syu
+	cd
 fi
 
-if ! [ -x "$(command -v htop)" ]; then
-	sudo pacman -S htop --noconfirm
+# Graphics Drivers find and install
+if lspci | grep -E "Integrated Graphics Controller"; then
+    pacman -S xf86-video-intel --needed --noconfirm
 fi
