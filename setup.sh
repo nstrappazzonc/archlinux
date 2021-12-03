b26bd60baa60eff19714d6448144a952f4546de8
Nicola Strappazzon C <nsc@nsc.local>
2021-12-03 22:36:03 +0100

    feat(configs): chown and chmod

--- setup.sh
+++ setup.sh
@@ -112,6 +112,8 @@ export BROWSER=firefox
 
 alias ll="ls -lahS"
 EOF
+chown root.root /etc/profile.d/*
+chmod 0644 /etc/profile.d/*
 
 /bin/cat > /etc/udev/rules.d/99-removable.rules << EOF
 ACTION=="add", SUBSYSTEM=="thunderbolt", ATTR{authorized}=="0", ATTR{authorized}="1"
@@ -126,6 +128,8 @@ EOF
 # Suspend the system when battery level drops to 5% or lower
 SUBSYSTEM=="power_supply", ATTR{status}=="Discharging", ATTR{capacity}=="[0-5]", RUN+="/usr/bin/systemctl hibernate"
 EOF
+chown root.root /etc/udev/rules.d/*
+chmod 0644 /etc/udev/rules.d/*
 
 udevadm control --reload-rules
 
@@ -134,6 +138,8 @@ GRUB_DEFAULT=0
 GRUB_TIMEOUT=0
 GRUB_RECORDFAIL_TIMEOUT=$GRUB_TIMEOUT
 EOF
+chown root.root /etc/default/grub.silent
+chmod 0644 /etc/default/grub.silent
 
 /bin/cat > /etc/X11/xorg.conf.d/30-touchpad.conf << EOF
 Section "InputClass"
@@ -143,14 +149,21 @@ Section "InputClass"
         Option "Tapping" "on"
 EndSection
 EOF
+chown root.root /etc/X11/xorg.conf.d/30-touchpad.conf
+chmod 0644 /etc/X11/xorg.conf.d/30-touchpad.conf
+
 
 /bin/cat > /etc/asound.conf << EOF
 defaults.pcm.card 1
 defaults.pcm.device 0
 defaults.ctl.card 0
 EOF
+chown root.root /etc/asound.conf
+chmod 0644 /etc/asound.conf
 
 /bin/cat > /etc/modprobe.d/hid_apple.conf << EOF
 options hid_apple fnmode=1
 options hid_apple iso_layout=0
 EOF
+chown root.root /etc/modprobe.d/hid_apple.conf
+chmod 0644 /etc/modprobe.d/hid_apple.conf
