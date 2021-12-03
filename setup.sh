ca66a26ed5ca509322a362bde5b3dfed8e9c8e75
Nicola Strappazzon C <nsc@nsc.local>
2021-12-03 21:41:39 +0100

    fix: use --noconfirm to install packages

--- setup.sh
+++ setup.sh
@@ -4,9 +4,9 @@ set -e
 sudo pacman -Syu --noconfirm
 
 if ! [ -x "$(command -v git)" ]; then
-	sudo pacman -S git
+	sudo pacman -S git --noconfirm
 fi
 
 if ! [ -x "$(command -v htop)" ]; then
-	sudo pacman -S htop
+	sudo pacman -S htop --noconfirm
 fi
