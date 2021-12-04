f5eb50079a6025ed389456a1c25ddb0254126c5e
Nicola Strappazzon C <nsc@nsc.local>
2021-12-05 00:23:18 +0100

    feat: sakura config

--- user.sh
+++ user.sh
@@ -13,16 +13,18 @@ git config --global user.name "Nicola Strappazzon C"
 git config pull.rebase true
 
 # Configure desktop
+mkdir -p ~/.config
+mkdir -p ~/.config/bspwm/
+mkdir -p ~/.config/flameshot/
+mkdir -p ~/.config/polybar/
+mkdir -p ~/.config/sakura/
+mkdir -p ~/.config/sxhkd/
 mkdir -p ~/Downloads
 mkdir -p ~/Pictures
-mkdir -p ~/.config
-mkdir -p ~/.config/polybar/
-mkdir -p ~/.config/bspwm/
-mkdir -p ~/.config/sxhkd/
-mkdir -p ~/.config/flameshot/
 
-cp config/xinitrc ~/.xinitrc
-cp config/polybar ~/.config/polybar/config
 cp config/bspwmrc ~/.config/bspwm/bspwmrc
-cp config/sxhkdrc ~/.config/sxhkd/sxhkdrc
 cp config/flameshot ~/.config/flameshot/flameshot.ini
+cp config/polybar ~/.config/polybar/config
+cp config/sakura ~/.config/sakura/sakura.conf
+cp config/sxhkdrc ~/.config/sxhkd/sxhkdrc
+cp config/xinitrc ~/.xinitrc
