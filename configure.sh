8a834c94599bf0adeb7eada7388b0c6cee96fd5c
Nicola Strappazzon C <nsc@nsc.local>
2021-12-04 16:39:54 +0100

    feat: implement flameshot

--- configure.sh
+++ configure.sh
@@ -7,6 +7,7 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
 
+# Locale
 if [[ -z "$(locale | grep LANG | cut -d= -f2)" ]]; then
     sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
     locale-gen
@@ -15,13 +16,13 @@ if [[ -z "$(locale | grep LANG | cut -d= -f2)" ]]; then
     localectl --no-ask-password set-locale LANG="en_US.UTF-8" LC_TIME="en_US.UTF-8" LANGUAGE="en_US"
 fi
 
-# Set keymaps
+# Keymaps
 localectl --no-ask-password set-keymap us
 
-# Add sudo rights
+# Sudo
 sed -i 's/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
 
-# Add custom profile
+# Profile
 /bin/cat > /etc/profile.d/custom.sh << EOF
 export GOPATH=$HOME/go
 export PATH=$PATH:$(go env GOPATH)/bin
@@ -34,6 +35,7 @@ EOF
 chown root.root /etc/profile.d/*
 chmod 0644 /etc/profile.d/*
 
+# Rules
 /bin/cat > /etc/udev/rules.d/99-removable.rules << EOF
 ACTION=="add", SUBSYSTEM=="thunderbolt", ATTR{authorized}=="0", ATTR{authorized}="1"
 EOF
@@ -50,6 +52,7 @@ EOF
 chown root.root /etc/udev/rules.d/*
 chmod 0644 /etc/udev/rules.d/*
 
+# grub
 /bin/cat > /etc/default/grub.silent << EOF
 GRUB_DEFAULT=0
 GRUB_TIMEOUT=0
@@ -58,6 +61,12 @@ EOF
 chown root.root /etc/default/grub.silent
 chmod 0644 /etc/default/grub.silent
 
+sed -i "s/timeout=5/timeout=0/" /boot/grub/grub.cfg
+sed -i "s/echo	'Loading Linux linux ...'//" /boot/grub/grub.cfg
+sed -i "s/echo	'Loading initial ramdisk ...'//" /boot/grub/grub.cfg
+sed -i "s/loglevel=3 quiet/quiet loglevel=0 rd.systemd.show_status=auto rd.udev.log_level=3/" /boot/grub/grub.cfg
+
+# Xorg
 /bin/cat > /etc/X11/xorg.conf.d/30-touchpad.conf << EOF
 Section "InputClass"
         Identifier "MyTouchpad"
@@ -69,6 +78,7 @@ EOF
 chown root.root /etc/X11/xorg.conf.d/30-touchpad.conf
 chmod 0644 /etc/X11/xorg.conf.d/30-touchpad.conf
 
+# Sound
 /bin/cat > /etc/asound.conf << EOF
 defaults.pcm.card 1
 defaults.pcm.device 0
@@ -77,6 +87,7 @@ EOF
 chown root.root /etc/asound.conf
 chmod 0644 /etc/asound.conf
 
+# Apple Keyboard
 /bin/cat > /etc/modprobe.d/hid_apple.conf << EOF
 options hid_apple fnmode=1
 options hid_apple iso_layout=0
