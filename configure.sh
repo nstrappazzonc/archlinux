ab0dba55709327a49072288c7c3e5545de420978
Nicola Strappazzon C <nsc@nsc.local>
2021-12-05 23:54:30 +0100

    shore(profile): more alias and define PS1 and other settings

--- configure.sh
+++ configure.sh
@@ -29,8 +29,14 @@ export PATH=$PATH:$(go env GOPATH)/bin
 export EDITOR=vim
 export TERM=xterm
 export BROWSER=firefox
+export CLICOLOR=1
+export LS_COLORS="di=1:fi=0:ln=31:pi=5:so=5:bd=5:cd=5:or=31"
+export PS1="\[\033[32m\]\W\[\033[31m\]\[\033[32m\]$\[\e[0m\]"
 
+alias ls="ls --color=auto"
 alias l="ls -lahS"
+alias grep="grep -n --color"
+alias diff="diff --color=auto"
 
 shortcuts () {
   echo "Shortcuts
