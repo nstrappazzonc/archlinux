7287f2c757c526d6d7e4a19ea1f3390b68d081d5
Nicola Strappazzon C <nsc@nsc.local>
2021-12-06 15:54:55 +0100

    fix(profile): auto include in .bashrc

--- user.sh
+++ user.sh
@@ -17,7 +17,7 @@ fi
 
 if test -d /etc/profile.d/; then
   for profile in /etc/profile.d/*.sh; do
-    test -r "$profile && . $profile"
+    test -r "$profile" && . "$profile"
   done
   unset profile
 fi
